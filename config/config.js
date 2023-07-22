0c5803b80513082761577858dd65131face225c3
dmkur <fgameua@gmail.com>
2023-07-22 15:18:03 +0300

    connected mongoose DB

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: process.env.PORT || 5001,
+  MONGO_URL: process.env.MONGO_URL || 'localhost:2700'
+};
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const { MONGO_URL, PORT } = require('./config/config');
+
+const app = express();
+
+mongoose.connect(MONGO_URL)
+// eslint-disable-next-line no-console
+  .then(() => console.log('DB connected')).catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running on port', PORT);
+});
