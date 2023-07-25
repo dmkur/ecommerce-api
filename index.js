a6844cdf6139abb39ae2e7a2f4d00c7e2565e5c4
dmkur <72811384+dmkur@users.noreply.github.com>
2023-07-25 12:05:17 +0000

    Update 10 files

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
-const { MONGO_URL, PORT } = require('./config/config');
+const { MONGO_URL, PORT } = require("./config/config");
+const { userRoute } = require("./routes");
 
 const app = express();
 
-mongoose.connect(MONGO_URL)
-// eslint-disable-next-line no-console
-  .then(() => console.log('DB connected')).catch((err) => console.log(err));
+mongoose
+  .connect(MONGO_URL)
+  // eslint-disable-next-line no-console
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/ping", (req, res) => {
+  res.json("pong");
+});
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is running on port', PORT);
+  console.log("Server is running on port", PORT);
 });
---
+++ models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  CartModel: require("./Cart"),
+  OrderModel: require("./Order"),
+  ProductModel: require("./Product"),
+  UserModel: require("./User"),
+};
