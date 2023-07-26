269e28e5158c20ea1f94d67bd42f8112689b490a
“dmkur” <fgameua@gmail.com>
2023-07-26 20:06:32 +0300

    add login and register function

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 dotenv.config();
-const { MONGO_URL, PORT } = require("./config/config");
-const { userRoute, authRoute } = require("./routes");
+const { MONGO_URL, PORT } = require('./config/config');
+const { userRoute, authRoute } = require('./routes');
 
 const app = express();
 
 mongoose
   .connect(MONGO_URL)
   // eslint-disable-next-line no-console
-  .then(() => console.log("DB connected"))
+  .then(() => console.log('DB connected'))
   .catch((err) => console.log(err));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/ping", (req, res) => {
-  res.json("pong");
+app.use('/ping', (req, res) => {
+  res.json('pong');
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log("Server is running on port", PORT);
+  console.log('Server is running on port', PORT);
 });
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,41 +5,54 @@ const { PAS_SEC } = require('../config/config');
 
 const authRouter = Router();
 
+// REGISTER
 authRouter.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(req.body.password, PAS_SEC).toString(),
+    password: CryptoJS.AES.encrypt(req.body.password, PAS_SEC)
+      .toString(),
   });
 
   try {
     const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+
+    res.status(201)
+      .json(savedUser);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500)
+      .json(err);
   }
 });
 
+// LOGIN
 authRouter.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-
     if (!user) {
-      res.status(401).json('Wrong credentials111');
+      res.status(401)
+        .json('Wrong credentials111');
       return;
     }
 
     const hashedPassword = CryptoJS.AES.decrypt(user.password, PAS_SEC);
 
-    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
-    if (password !== req.body.password) {
-      res.status(401).json('Wrong credentials222');
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    if (originalPassword !== req.body.password) {
+      res.status(401)
+        .json('Wrong credentials222');
       return;
     }
 
-    res.json(user);
+    // eslint-disable-next-line no-unused-vars
+    const { password, ...other } = user._doc;
+    // чомусь mongoose зберігає цілий файл з різними данними
+    // а наш юзер в полі _doc тому доступаємся так
+
+    res.json(other);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500)
+      .json(err);
   }
 });
 
