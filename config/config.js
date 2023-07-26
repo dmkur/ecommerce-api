d709a550a590a10b8f26ab8a4f057f679ae7d240
“dmkur” <fgameua@gmail.com>
2023-07-26 20:32:03 +0300

    add jwt

--- config/config.js
+++ config/config.js
@@ -2,4 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 5001,
   MONGO_URL: process.env.MONGO_URL || 'localhost:2700',
   PAS_SEC: process.env.PAS_SEC || 'dmkur',
+
+  JWT_SEC: process.env.JWT_SEC || 'dmkur',
+  ACCESS_TOKEN_LIFETIME: process.env.ACCESS_TOKEN_LIFETIME || '3d'
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,12 @@
 const { Router } = require('express');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
-const { PAS_SEC } = require('../config/config');
+const {
+  PAS_SEC,
+  JWT_SEC,
+  ACCESS_TOKEN_LIFETIME
+} = require('../config/config');
 
 const authRouter = Router();
 
@@ -44,12 +49,17 @@ authRouter.post('/login', async (req, res) => {
       return;
     }
 
+    // у payload додаємо два праметри, щоб потім їх використати при аунтифікації юзера і його прав
+    const accessToken = jwt.sign({
+      id: user._id, isAdmin: user.isAdmin
+    }, JWT_SEC, { expiresIn: ACCESS_TOKEN_LIFETIME });
+
     // eslint-disable-next-line no-unused-vars
     const { password, ...other } = user._doc;
     // чомусь mongoose зберігає цілий файл з різними данними
     // а наш юзер в полі _doc тому доступаємся так
 
-    res.json(other);
+    res.json({ ...other, accessToken });
   } catch (err) {
     res.status(500)
       .json(err);
