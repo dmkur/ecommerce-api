44435dd894c16f0dc12f019192528f6beee0f54c
dmkur <72811384+dmkur@users.noreply.github.com>
2023-07-26 10:36:02 +0000

    Update 4 files

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
+const CryptoJS = require("crypto-js");
 const { User } = require("../models");
+const { PAS_SEC } = require("../config/config");
 
 const authRouter = Router();
 
@@ -7,12 +9,35 @@ authRouter.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, PAS_SEC).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+ 
+    if (!user) {
+      res.status(401).json("Wrong credentials111");
+      return;
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, PAS_SEC);
+
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    if (password !== req.body.password) {
+      res.status(401).json("Wrong credentials222");
+      return;
+    }
+
+    res.json(user);
   } catch (err) {
     res.status(500).json(err);
   }
