a90eef62d17b425c3f3eece05dc0aa7298a17b46
dmkur <72811384+dmkur@users.noreply.github.com>
2023-07-26 07:57:31 +0000

    Update 5 files

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
-const { userController } = require("../controllers");
+const { User } = require("../models");
 
 const authRouter = Router();
 
-authRouter.post("/register", userController.createUser);
+authRouter.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = authRouter;
--- services/user.service.js
+++
@@ -1,5 +0,0 @@
-const { User } = require("../models");
-
-module.exports = {
-  create: (userObject) => User.create(userObject),
-};
