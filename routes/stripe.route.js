e53bbf1d35ea1cf9024111b94a6719602049380b
“dmkur” <fgameua@gmail.com>
2023-07-28 17:27:43 +0300

    add stripe

--- routes/stripe.route.js
+++ routes/stripe.route.js
@@ -7,12 +7,12 @@ stripeRouter.post('/payment', (req, res) => {
   stripe.charges.create({
     source: req.body.tokenId,
     amount: req.body.amount,
-    currency:"usd"
-  },(stripeErr, stripeRes)=>{
-    if(stripeErr){
-      res.status(500).json(stripeErr)
-    }else{
-      res.json(stripeRes)
+    currency: 'usd'
+  }, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).json(stripeErr);
+    } else {
+      res.json(stripeRes);
     }
   });
 });
