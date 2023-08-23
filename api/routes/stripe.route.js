eddc09c38c702a87c72afe9724a9676cead8781e
“dmkur” <fgameua@gmail.com>
2023-08-23 19:52:14 +0300

    add stripe method

--- api/routes/stripe.route.js
+++ api/routes/stripe.route.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const stripeRouter = Router();
 
-stripeRouter.post('/payment', (req, res) => {
-  stripe.charges.create({
+stripeRouter.post('/payment', async (req, res) => {
+  await stripe.charges.create({
     source: req.body.tokenId,
     amount: req.body.amount,
     currency: 'usd'
