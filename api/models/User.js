947096bdccd226d2bc85538c72cca3b002d2fd69
“dmkur” <fgameua@gmail.com>
2023-09-04 20:12:28 +0300

    add img in User.js model

--- api/models/User.js
+++ api/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
+    img: { type: String }
   },
   { timestamps: true }
 );
