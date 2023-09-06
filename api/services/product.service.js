39fec4794888875ea776454c02bd669ff1bfb017
“dmkur” <fgameua@gmail.com>
2023-09-06 19:44:16 +0300

    update delete method

--- api/services/product.service.js
+++ api/services/product.service.js
@@ -3,7 +3,7 @@ const { Product } = require('../models');
 module.exports = {
   create: (productObj) => Product.create(productObj),
   updateById: (id, objectToUpdate) => Product.findByIdAndUpdate({ _id: id }, objectToUpdate, { new: true }),
-  deleteById: (id) => Product.findOneAndDelete(id),
+  deleteById: (id) => Product.findByIdAndDelete(id),
   getById: (id) => Product.findById(id),
   find: (filter = {}) => Product.find(filter)
 };
